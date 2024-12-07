2559edce8357314a9bfe8ba4baf8778407ff6822
Shreyas Jenner <shreyasjenner4@gmail.com>
2024-12-07 14:56:18 +0530

    mark attendance page partially completed

--- attendance-app/app/MarkAttendance.tsx
+++ attendance-app/app/MarkAttendance.tsx
@@ -1,12 +1,151 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import * as DatabaseCRUD from '../components/DatabaseCRUD';
+import { Dropdown, MultiSelect } from 'react-native-element-dropdown';
+import { Calendar } from 'react-native-calendars';
 
 const MarkAttendance = () => {
+  const [coursecode, setCoursecode] = useState<DatabaseCRUD.Course[]>([]);
+
+  // states to store selected course and check if it has been filled
+  const [selectedcourse, setCourseValue] = useState('');
+  const [courseFocus, setCourseFocus] = useState(false);
+
+  // state to store selected periods and check if it has been filled
+  const [selectedperiods, setSelectedPeriods] = useState([]);
+  
+  // state to store selected date
+  const [selecteddates, setDate] = useState('');
+
+  // const list to store slots and associated values
+  const periods = [
+    {label: 'Slot 1', value:'1'},
+    {label: 'Slot 2', value:'2'},
+    {label: 'Slot 3', value:'3'},
+    {label: 'Slot 4', value:'4'},
+    {label: 'Slot 5', value:'5'},
+    {label: 'Slot 6', value:'6'},
+    {label: 'Slot 7', value:'7'},
+    {label: 'Slot 8', value:'8'},
+    {label: 'Slot 9', value:'9'},
+    {label: 'Slot 10', value:'10'},
+    {label: 'Slot 11', value:'11'},
+    {label: 'Slot 12', value:'12'},
+  ];
+
+  // useEffect that runs at start of loading of page, to get all course data
+  useEffect(() => {
+
+    // Function to get list of courses
+    const FetchData = async () => {
+      try {
+        const db = await DatabaseCRUD.openDatabase();
+        if(db) {
+          const res:DatabaseCRUD.Course[] = await DatabaseCRUD.getAllCourses(db);
+          
+          // fill coursecode state with course_code obtained from res
+          setCoursecode(res);
+
+        } else {
+          console.error("Error opening databse");
+        }
+      } catch(err) {
+        console.error("Error fetching data");
+      }
+    }
+
+    FetchData();
+  }, []);
+
+  // function to mark attendance and store data in database
+  const markAttendance = async () => {
+
+    if((selectedcourse == '') || (selectedperiods.length == 0) || (selecteddates == '')) {
+      console.error("Fill all fields");
+    } else {
+      //TODO: push all data to database
+
+      // set all input arrays to empty
+      setCourseValue('Course Code');
+      setSelectedPeriods([]);
+      setDate('');
+    }
+  }
+
+  // function to render selected periods in multi-selected prop
+  const renderSelectedPeriods = () => {
+    return selectedperiods.join(', ');
+  }
+
   return (
-    <View>
-      <Text>MarkAttendance</Text>
+    <View style={styles.containerStyle}>
+      <View style={styles.inputContainerStyle}>
+        {/* Dropdown for course code */}
+        <Dropdown 
+        style={styles.dropdownStyle}
+        data={coursecode} 
+        search
+        labelField="course_name"
+        valueField="course_code"
+        value={selectedcourse}
+        onChange={item => {setCourseValue(item.course_code); setCourseFocus(false)}}
+        onFocus={() => setCourseFocus(true)}
+        placeholder={!courseFocus? 'Course Code' : ''}
+        searchPlaceholder='Search...'
+        />
+
+        {/* Dropdown for period */}
+        <MultiSelect 
+        style={styles.dropdownStyle}
+        search
+        data={periods}
+        labelField='label'
+        valueField='value'
+        placeholder={(selectedperiods.length == 0)?'Select Periods':renderSelectedPeriods()}
+        searchPlaceholder='Search...'
+        value={selectedperiods}
+        alwaysRenderSelectedItem={false}
+        visibleSelectedItem={false}
+        onChange={item => {setSelectedPeriods(item)}}
+        />
+
+      </View>
+
+      <View style={styles.inputContainerStyle}>
+        {/* Calendar for data selection */}
+        <Calendar
+        style={styles.calendarStyle}
+        onDayPress={day => {setDate(day.dateString)}}
+        markedDates={{[selecteddates]: {selected: true, disableTouchEvent: true}}}
+        />
+      </View>
+
+      <View style={styles.inputContainerStyle}>
+        <Button title='Mark Attendance' onPress={markAttendance}/>
+      </View>
     </View>
   )
 }
 
-export default MarkAttendance
+export default MarkAttendance;
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
+  },
+  inputContainerStyle: {
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  dropdownStyle: {
+    flex: 1,
+  },
+  calendarStyle: {
+    
+  }
+})
--- attendance-app/components/DatabaseCRUD.ts
+++ attendance-app/components/DatabaseCRUD.ts
@@ -15,6 +15,31 @@ interface TimeStamp {
   course_code: string
 }
 
+// queries to get data from database
+const createTablesQuery = `
+      PRAGMA journal_mode = WAL;
+      CREATE TABLE IF NOT EXISTS courses (
+        course_code varchar(8) PRIMARY KEY,
+        course_name varchar(40),
+        total_days INTEGER
+      );
+      CREATE TABLE IF NOT EXISTS timestamps (
+        timestamp_id INTEGER PRIMARY KEY,
+        period INTEGER,
+        day TEXT,
+        course_code varchar(8),
+        FOREIGN KEY(course_code) REFERENCES courses(course_code)
+      );`;
+
+const insertCourseDataQuery = 'INSERT OR IGNORE INTO courses (course_code, course_name, total_days) VALUES (?, ?, ?)';
+
+const getAllCoursesQuery = 'SELECT * FROM courses';
+
+const getAllTimestampsQuery = 'SELECT * FROM timestamps';
+
+const clearTableQuery = (tableName: string) => `DELETE FROM ${tableName}`;
+
+
 // Function to open a database connection
 const openDatabase = async (): Promise<SQLite.SQLiteDatabase | undefined> => {
   try {
@@ -30,21 +55,7 @@ const openDatabase = async (): Promise<SQLite.SQLiteDatabase | undefined> => {
 // Function to create the tables in the database
 const createTables = async (db: SQLite.SQLiteDatabase): Promise<void> => {
   try {
-    await db.execAsync(`
-      PRAGMA journal_mode = WAL;
-      CREATE TABLE IF NOT EXISTS courses (
-        course_code varchar(8) PRIMARY KEY,
-        course_name varchar(40),
-        total_days INTEGER
-      );
-      CREATE TABLE IF NOT EXISTS timestamps (
-        timestamp_id INTEGER PRIMARY KEY,
-        period INTEGER,
-        day TEXT,
-        course_code varchar(8),
-        FOREIGN KEY(course_code) REFERENCES courses(course_code)
-      );
-    `);
+    await db.execAsync(createTablesQuery);
     console.log("Tables created (if not exists)!");
   } catch (error) {
     console.error("Failed to create tables:", error);
@@ -54,8 +65,7 @@ const createTables = async (db: SQLite.SQLiteDatabase): Promise<void> => {
 // Function to insert data into the courses tables
 const insertCourseData = async (db: SQLite.SQLiteDatabase, course_code: string, course_name: string, total_days: number): Promise<void> => {
   try {
-    const res = await db.runAsync('INSERT OR IGNORE INTO courses (course_code, course_name, total_days) VALUES (?, ?, ?)', 
-      course_code, course_name, total_days);
+    await db.runAsync(insertCourseDataQuery,course_code, course_name, total_days);
 
     // console.log(res);
   } catch (error) {
@@ -79,7 +89,7 @@ const insertTimestampData = async (db: SQLite.SQLiteDatabase, timestamp_id: numb
 // Function to fetch all courses
 const getAllCourses = async (db: SQLite.SQLiteDatabase): Promise<any[]> => {
   try {
-    const result = await db.getAllAsync('SELECT * FROM courses');
+    const result = await db.getAllAsync(getAllCoursesQuery);
     return result;
   } catch (error) {
     console.error("Failed to fetch courses:", error);
@@ -90,7 +100,7 @@ const getAllCourses = async (db: SQLite.SQLiteDatabase): Promise<any[]> => {
 // Function to fetch all timestamps
 const getAllTimestamps = async (db: SQLite.SQLiteDatabase): Promise<any[]> => {
   try {
-    const result = await db.getAllAsync('SELECT * FROM timestamps');
+    const result = await db.getAllAsync(getAllTimestampsQuery);
     return result;
   } catch (error) {
     console.error("Failed to fetch timestamps:", error);
@@ -101,7 +111,7 @@ const getAllTimestamps = async (db: SQLite.SQLiteDatabase): Promise<any[]> => {
 // Function to delete all rows in a table
 const clearTable = async (db: SQLite.SQLiteDatabase, tableName: string): Promise<any[]> => {
   try {
-    const res = await db.getAllAsync(`DELETE FROM ${tableName}`);
+    const res = await db.getAllAsync(clearTableQuery(tableName));
     return res;
   } catch(err) {
     console.error("Failed to delete table:", err);
